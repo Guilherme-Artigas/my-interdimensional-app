e485ad382b1506cbd3e9067db20a628adc4789c1
Guilherme Artigas <guilherme.artigas92@gmail.com>
2022-08-26 19:22:35 -0300

    Exercicios do dia 12-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import DadJoke from './components/DadJoke';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      characters: [],
+    };
+  }
+
+  // Existem duas formas de se trabalhar com requisições em React...
+  // 1º...
+  // fetchCharacters = () => {
+  //   fetch('https://rickandmortyapi.com/api/character')
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       this.setState({characters: data.results});
+  //     });
+  // };
+
+  // 2º Forma...
+  async componentDidMount() {
+    // this.fetchCharacters();
+    const response = await fetch('https://rickandmortyapi.com/api/character')
+    const data = await response.json();
+    this.setState({characters: data.results});
+  };
+
+  render() {
+    // const { characters } = this.state;
+    return (
+      // <div className="App">
+      //   <h1>Rick and Morty Characters:</h1>
+      //   <div className="body">
+      //     {characters.map(({name, image}) => {
+      //       return (
+      //         <div className="container" key={name}>
+      //           <h3>{name}</h3>
+      //           <img src={image} alt={name} />
+      //         </div>
+      //       );
+      //     })}
+      //   </div>
+      // </div>
+      <DadJoke /> // Conteúdo da aula: Renderização condicional e atualização de arrays no estado
+    );
+  }
 }
 
 export default App;
+
+// Código exemplo da aula 'Requisições e componentDidMount', aonde fazemos a renderização de uma API de piadas
+
+// import React from 'react';
+
+// class DadJoke extends React.Component {
+//   constructor() {
+//     super();
+
+//     this.saveJoke = this.saveJoke.bind(this);
+
+//     this.state = {
+//       jokeObj: undefined,
+//       loading: true,
+//       storedJokes: [],
+//     }
+//   }
+
+//   async fetchJoke() {
+//     const requestHeaders = { headers: { Accept: 'application/json' } }
+//     const requestReturn = await fetch('https://icanhazdadjoke.com/', requestHeaders)
+//     const requestObject = await requestReturn.json();
+//     this.setState({
+//       jokeObj: requestObject,
+//     })
+//   }
+
+//   componentDidMount() {
+//     this.fetchJoke();
+//   }
+
+//   saveJoke() {
+//     //Esse método será responsável por salvar a piada no array de piadas storedJokes!!
+//   }
+
+//   render() {
+//     const { storedJokes } = this.state;
+//     const loadingElement = <span>Loading...</span>;
+
+//     return (
+//       <div>
+//         <span>
+//           {storedJokes.map(({ id, joke }) => (<p key={id}>{joke}</p>))}
+//         </span>
+
+//       {
+//         /*
+//         Aqui vamos construir nossa lógica com uma renderização condicional
+//         do nosso componente Joke, a ideia é renderizar um loading enquanto
+//         esperamos a nossa requisição de piadas finalizar.
+
+//         <p>RENDERIZAÇÃO CONDICIONAL</p>
+//         */
+//       }
+
+//       </div>
+//     );
+//   }
+// }
+
+// export default DadJoke;
+
---
+++ src/components/Joke.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Joke extends React.Component {
+  render() {
+    const { jokeObj, saveJoke } = this.props;
+    return (
+      <div>
+        <p>{jokeObj.Obj}</p>
+        <button
+          type="button"
+          onClick={saveJoke}
+        >
+          Salvar Piada!
+        </button>
+      </div>
+    );
+  };
+};
+
+export default Joke;
